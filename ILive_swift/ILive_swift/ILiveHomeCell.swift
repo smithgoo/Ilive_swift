8e847d9e06ab9bfbd484c8da4ec4751fe284f332
294336370@qq.com <294336370@qq.com>
2021-03-20 20:34:48 +0800

    fixc

---
+++ ILive_swift/ILive_swift/ILiveHomeCell.swift
@@ -0,0 +1,12 @@
+//
+//  ILiveHomeCell.swift
+//  ILive_swift
+//
+//  Created by 王朋 on 2021/3/20.
+//
+
+import UIKit
+
+class ILiveHomeCell: UITableViewCell {
+     
+}
--- ILive_swift/ILive_swift/ILiveHomeViewController.swift
+++ ILive_swift/ILive_swift/ILiveHomeViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
-class ILiveHomeViewController: UIViewController {
-
+class ILiveHomeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell.init()
+    }
+    
+    var tableView:UITableView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.setupUI()
+    }
+    
+    private func setupUI() {
+        self.tableView = UITableView.init()
+        self.view.addSubview(self.tableView!)
+        self.tableView?.snp.makeConstraints({ make in
+            make.edges.equalTo(self.view)
+        })
     }
     
 }
+
+ 
